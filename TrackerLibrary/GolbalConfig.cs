177a8c930907f5b45eb336a718f91293a5328ea5
username-naz <duttaaniruddha2001@gmail.com>
2021-06-23 20:09:18 +0530

    grouped data connections and models

--- TrackerLibrary/GolbalConfig.cs
+++ TrackerLibrary/GolbalConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TrackerLibrary.DataAccess;
 
 namespace TrackerLibrary
 {
--- TrackerUI/CreatePrizeForm.cs
+++ TrackerUI/CreatePrizeForm.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using TrackerLibrary;
+using TrackerLibrary.Models;
 
 namespace TrackerUI
 {
